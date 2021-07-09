b699744e1bcd943cd1605a92941981dd8efb3c4a
umsenpaiprogramador <78985331+umsenpaiprogramador@users.noreply.github.com>
2021-07-08 21:47:56 -0300

    Add files via upload
    Aula que ministrei sobre Desenvolvimento de Software (DS) do curso ETIM de Informática.
    Fiz algumas melhorias em relação as aulas.
    -Sistema básico com CRUD
    -WebService para busca automática de CEP
    -Relatórios com JasperSoft Studio.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Sistema-de-Vendas
-Sistema de Vendas com Java Desktop e Mysql
+# Projeto-Vendas-Curso-Lojinha
+ Sistema de Vendas - Aula de Desenvolvimento de Software
+ 
+ Etim de Informática
+ 
+ Sistema de vendas (Módulo Clientes)
+ -Cadastra
+ -Exclui
+ -Altera
+ -Busca no Banco
+ -Limpa Formulário
+ -Gera Relatório (Jaspersoft Studio)
+ -Busca Cep com Webservice
+ 
+
+ 
---
+++ src/br/com/projeto/dao/FuncionariosDAO.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projeto.dao;
+
+import br.com.projeto.jdbc.ConnectionFactory;
+import br.com.projeto.model.Funcionarios;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author programador
+ */
+public class FuncionariosDAO {
+    
+    //Conexão com Banco de Dados
+    private Connection con;
+
+    public FuncionariosDAO() {
+        this.con = new ConnectionFactory().getConnection();
+    }
+    
+    //Método Cadastrar Funcionário
+    public void cadastrarFuncionarios(Funcionarios obj) {
+        try {
+
+            //1 passo  - criar o comando sql
+            String sql = "insert into tb_funcionarios (nome,rg,cpf,email,senha, cargo, nivel_acesso, telefone,celular,cep,endereco,numero,complemento,bairro,cidade,estado) "
+                    + " values (?,?,?,?,?,?,?,?,?,?,?,?,?, ?, ?, ?)";
+
+            //2 passo - conectar o banco de dados e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getRg());
+            stmt.setString(3, obj.getCpf());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5, obj.getSenha());
+            stmt.setString(6, obj.getCargo());
+            stmt.setString(7, obj.getNivel_acesso());
+            stmt.setString(8, obj.getTelefone());
+            stmt.setString(9, obj.getCelular());
+            stmt.setString(10, obj.getCep());
+            stmt.setString(11, obj.getEndereco());
+            stmt.setInt(12, obj.getNumero());
+            stmt.setString(13, obj.getComplemento());
+            stmt.setString(14, obj.getBairro());
+            stmt.setString(15, obj.getCidade());
+            stmt.setString(16, obj.getUf());
+
+            //3 passo - executar o comando sql
+            stmt.execute();
+            stmt.close();
+
+            JOptionPane.showMessageDialog(null, "Cadastrado com Sucesso!");
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro: " + erro);
+
+        }
+
+    }
+    
+}
